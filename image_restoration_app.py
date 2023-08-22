5ba63751c18e0d987c625d69fb5638a320de41ef
Emd956 <109397313+Emd956@users.noreply.github.com>
2023-08-22 00:22:57 -0600

    Update image_restoration_app.py

--- image_restoration_app.py
+++ image_restoration_app.py
@@ -16,10 +16,9 @@ STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
 
 # We create a downloads directory within the streamlit static asset directory
 # and we write output files to it.
-DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
+DOWNLOADS_PATH = pathlib.Path(__file__).parent / "downloads"
 if not DOWNLOADS_PATH.is_dir():
     DOWNLOADS_PATH.mkdir()
-
 
 def get_image_download_link(img, filename, text):
     """Generates a link to download a particular image file."""
